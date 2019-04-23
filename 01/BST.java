e9e4ebd8f07f4bac9c48bb376440a5c5be887feb
c212 <40395648+c212@users.noreply.github.com>
2019-04-23 14:03:28 -0400

    find is finished
    is it efficient though?

--- 01/BST.java
+++ 01/BST.java
@@ -35,10 +35,14 @@ public class BST<T extends Comparable<T>> {
     if (this.root == null) return 0; 
     else return 1 + this.root.left.size() + this.root.right.size(); 
   }
-  public T find(int k) {
+  public T find(int k) { // k is 0 based 
+    // System.out.println("Looking for element index " + k + " in tree " + this.print()); 
     if (k < 0) return null; 
     else if (k >= this.size()) return null; 
-    return null; 
+    else if (k < this.root.left.size()) return this.root.left.find(k);  
+    else if (k == this.root.left.size()) return this.root.value;  
+    else // if k > this.root.left.size() 
+      return this.root.right.find( k - this.root.left.size() - 1); 
   }
   
   public static void main(String[] args) {
@@ -52,10 +56,9 @@ public class BST<T extends Comparable<T>> {
     a.insert( 7 ); 
     a.insert(14); 
     a.insert(13 ); 
-    System.out.println( a ); // expect: 1 3 4 6 8 10  
-    System.out.println( a.print() ); // expect: (8 (3 (1  . .) (6 (4  . .) . )) (10  . .))
-                                             // (8 (3 (1 ()()) (6 (4 ()()) ())) (10 ()())) 
-    System.out.println( a.find(3) ); // null 
+    System.out.println( a ); // expect: 1 3 4 6 7 8 10 13 14 
+    System.out.println( a.find(3) ); // 6
+    System.out.println( a.find(7) ); // 13
     System.out.println( a.find(-2) ); // null 
     System.out.println( a.find(10) ); // null 
   }
