b3a295d75cd479927fe6330f5e56adcf99238dc3
c212 <40395648+c212@users.noreply.github.com>
2019-04-25 14:17:12 -0400

    Add files via upload

--- 03/EmptyList.java
+++ 03/EmptyList.java
@@ -2,9 +2,12 @@ public class EmptyList implements LispList {
   public boolean empty() { return true; }
   // https://docs.oracle.com/javase/8/docs/api/java/lang/UnsupportedOperationException.html
   public Object first() throws UnsupportedOperationException { 
+    // Elle: it compiles without throws why is that? 
     throw new UnsupportedOperationException(); 
   }
-  public LispList rest() throws UnsupportedOperationException { return null; } 
+  public LispList rest() throws UnsupportedOperationException { 
+    throw new UnsupportedOperationException(); 
+  } 
   public static void main(String[] args) {
     EmptyList a = new EmptyList(); 
     System.out.println( a ); 
@@ -13,7 +16,7 @@ public class EmptyList implements LispList {
     } catch (UnsupportedOperationException e) {
       System.out.println( "An empty list has no first." ); 
     }
-    System.out.println( a.rest() ); 
+    // System.out.println( a.rest() ); 
     System.out.println( a.empty() ); // true 
     
   }
--- 03/NonEmptyList.java
+++ 03/NonEmptyList.java
@@ -1,12 +1,23 @@
 public class NonEmptyList implements LispList {
+  public NonEmptyList(Object first, LispList rest) {
+    this.first = first;
+    this.rest = rest; 
+  }
+  public String toString() {
+    return this.first + ", " + this.rest;  
+  }
+  private Object first; // head
+  private LispList rest; // tail
   public boolean empty() { return false; }
   public Object first() { return null; }
   public LispList rest() { return null; }
   public static void main(String[] args) {
-    NonEmptyList a; 
-    a = new NonEmptyList(); 
+    LispList a;
+    a = new NonEmptyList("Julia", new EmptyList()); 
     System.out.println( a ); 
-    System.out.println( a.empty() ); 
-    
+    a = new NonEmptyList("Kendall", a); 
+    System.out.println( a ); 
+    a = new NonEmptyList( "Sultan", new NonEmptyList( "Alex", new NonEmptyList( "Julia", new EmptyList() ) ) );
+    System.out.println( a ); 
   }
 }
