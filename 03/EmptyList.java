598313d9703a7ddc2068bf144633548f18722a47
c212 <40395648+c212@users.noreply.github.com>
2019-04-25 13:39:10 -0400

    Add files via upload

--- 03/EmptyList.java
+++ 03/EmptyList.java
@@ -1,5 +1,13 @@
 public class EmptyList implements LispList {
-  public boolean empty() { return false; }; 
+  public boolean empty() { return true; }; 
   public Object first() { return null; }
   public LispList rest() { return null; } 
+  public static void main(String[] args) {
+    EmptyList a = new EmptyList(); 
+    System.out.println( a ); 
+    System.out.println( a.first() ); 
+    System.out.println( a.rest() ); 
+    System.out.println( a.empty() ); // true 
+    
+  }
 }
